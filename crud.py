084933e39f080e45a7792faaf9209dcdfc62b7a5
Your Name <rachelbarkanic@Rachels-MacBook-Air.local>
2023-02-07 14:34:35 -0800

    need to add html for users next

--- crud.py
+++ crud.py
@@ -13,6 +13,16 @@ def create_user(email, password):
 
     return user
 
+def get_users():
+    '''Return all users'''
+
+    return User.query.all()
+
+def get_user_by_id(user_id):
+    '''Return user based on ID'''
+
+    return User.query.get(user_id)
+
 def create_movie(title, overview, release_date, poster_path):
     '''Create and return a new movie'''
 
@@ -25,6 +35,16 @@ def create_movie(title, overview, release_date, poster_path):
 
     return movie
 
+def get_movies():
+    '''Return all movies'''
+
+    return Movie.query.all()
+
+def get_movie_by_id(movie_id):
+    '''Return movie based on ID'''
+
+    return Movie.query.get(movie_id)
+
 def create_rating(user, movie, score):
     '''Create and return a new rating'''
 
@@ -35,6 +55,8 @@ def create_rating(user, movie, score):
         )
 
     return rating
+
+
 
 if __name__ == '__main__':
     from server import app
