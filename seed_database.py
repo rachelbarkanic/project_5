bacd8eae5a5e863bf5915415c94adc696819458b
Your Name <rachelbarkanic@Rachels-MacBook-Air.local>
2023-02-09 19:32:22 -0800

    need to add update user, make prettier

--- seed_database.py
+++ seed_database.py
@@ -5,8 +5,9 @@ import json
 from random import choice, randint
 from datetime import datetime
 
-import crud
 import model
+
+from model import connect_to_db, db, User, Movie, Rating
 import server
 
 os.system('dropdb ratings')
@@ -32,7 +33,7 @@ for movie in movie_data:
 
     release_date = datetime.strptime(movie['release_date'], '%Y-%m-%d')
 
-    db_movie = crud.create_movie(title, overview, release_date, poster_path)
+    db_movie = Movie.create_movie(title, overview, release_date, poster_path)
     movies_in_db.append(db_movie)
 
 model.db.session.add_all(movies_in_db)
@@ -47,14 +48,14 @@ for n in range(10):
     email = f'user{n}@test.com'
     password = 'test'
 
-    user = crud.create_user(email, password)
+    user = User.create_user(email, password)
     model.db.session.add(user)
 
     for _ in range(10):
         random_movie = choice(movies_in_db)
         score = randint(1, 5)
 
-        rating = crud.create_rating(user, random_movie, score)
+        rating = Rating.create_rating(user, random_movie, score)
         model.db.session.add(rating)
 
 model.db.session.commit()
